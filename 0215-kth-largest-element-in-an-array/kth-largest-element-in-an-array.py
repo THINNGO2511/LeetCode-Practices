e00eb035e60c4e5ef0158457ca7d6ef557ed315d
Edward Ngo <55904076+THINNGO2511@users.noreply.github.com>
2025-03-28 01:07:13 -0500

    Time: 46 ms (88.15%) Space: 28.7 MB (47.33%)

--- 0215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -3,33 +3,6 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        #sort sol (cheating)
-        # nums.sort(reverse=True)
-        # return nums[k-1]
-
-        #bin search sol:
-        # Set the boundaries for binary search, use min and max to define the range of possible values where the kth largest element might be
-        # l, r = min(nums), max(nums)
-        
-        # while l <= r:
-        #     mid = (l + r) // 2
-        #     # Count how many elements are greater than or equal to 'mid'
-        #     count = sum(x >= mid for x in nums)
-        #     print(count)
-            
-        #     if count >= k:
-        #         # If there are at least 'k' elements >= mid, the kth largest is >= mid
-        #         l = mid + 1
-        #     else:
-        #         # If there are fewer than 'k' elements >= mid, reduce the upper boundary
-        #         r = mid - 1
-                
-        # return r  # When l > r, 'r' will hold the value of the kth largest element
-        #l and r are merely indicator of number to find on array, not setting physical search boundaries
-
-        #time: O(nlogn)
-        #space: 0(1)
-
         #or we can do heap
         heap = nums[:k] #if k = 4, take first 4 element (this seves as a window)
         heapq.heapify(heap)#heapify to sort, this is min-heap
